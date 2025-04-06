d5bfd7e777d4f4308ecd54b0125812233c1edb5d
Juan Carlos Olivares Rojas <juan.or@morelia.tecnm.mx>
2025-04-06 08:13:50 -0600

    Primera version de Programación Backend

---
+++ buscar.php
@@ -0,0 +1,20 @@
+<?php
+$serie = $_GET["serie"];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header id="encabezado">
+        <h1><?=$serie;?></h1>
+        <hr>
+    </header>
+</body>
+</html>
+
+
+
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>La fecha del sistema:
+        <?php
+        echo date("Y-m-d h:m:s");
+        ?>
+    </p>
+    <hr>
+    <form action="buscar.php" method="get">
+        <label for="serie">No. de serie</label>
+        <input type="text" name="serie" id="serie" placeholder="Coloca el seriado de tu billete">
+        <input type="submit" value="Enviar">
+    </form>
+
+</body>
+</html>
